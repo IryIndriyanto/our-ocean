db7ca9774a71e82d5c7de9b5a6728136d5ffe589
PutriNoviani <ptrnov0802@gmail.com>
2024-04-27 13:54:18 +0700

    New Init

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,15 +7,6 @@ import {
   MdBookmark,
 } from 'react-icons/md';
 
-// Admin Imports
-// import MainDashboard from './pages/admin/default';
-// import NFTMarketplace from './pages/admin/nft-marketplace';
-// import Profile from './pages/admin/profile';
-// import DataTables from './pages/admin/data-tables';
-// import RTL from './pages/rtl/rtl-default';
-
-// Auth Imports
-// import SignInCentered from './pages/auth/sign-in';
 import { IRoute } from '@/types/navigation';
 
 const routes: IRoute[] = [
