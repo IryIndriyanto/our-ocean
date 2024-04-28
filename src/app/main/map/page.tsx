303729cea864e3250fc89c6c6beb5049ce5d961e
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-04-27 20:57:05 -0700

    integrating leaflet map component

---
+++ src/app/main/map/page.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import Map from '@/components/maps/Map';
+import {
+  Box,
+  Flex,
+  FormLabel,
+  Image,
+  Icon,
+  Select,
+  SimpleGrid,
+  useColorModeValue,
+} from '@chakra-ui/react';
+
+export default function Default() {
+  // Chakra Color Mode
+
+  return (
+    <Map/>
+  );
+}
---
+++ src/components/maps/Map.tsx
@@ -0,0 +1,52 @@
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+  ZoomControl,
+} from "react-leaflet";
+import { icon } from "leaflet";
+
+export default function Map() {
+  const ICON = icon({
+    iconUrl: "/marker.png",
+    iconSize: [30, 40],
+  });
+
+  return (
+    <MapContainer
+      style={{ height: "100vh", zIndex: "0" }}
+      center={[51.505, -0.09]}
+      zoom={13}
+      zoomControl={false}
+    >
+      <ClickComponent />
+      <ZoomControl position="bottomright"/>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]} icon={ICON}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
+
+function ClickComponent() {
+  const map = useMapEvents({
+    click: (e) => {
+      const { lat, lng } = e.latlng;
+      console.log("Clicked location:", lat, lng);
+      map.locate();
+    },
+    locationfound: (location) => {
+      console.log("location found:", location);
+      // map.setView(location.latlng, 12)
+    },
+  });
+  return null;
+}
