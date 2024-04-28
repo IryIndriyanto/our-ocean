ca8593268f2e0b16bec194397384b6a8dd2c6261
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-04-28 03:33:19 -0700

    add bookmarks page

---
+++ src/app/main/bookmarks/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import {
+  Box,
+  Flex,
+  FormLabel,
+  Image,
+  Icon,
+  Select,
+  SimpleGrid,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
+export default function Default() {
+  // Chakra Color Mode
+
+  return (
+    <>
+      <div>this is bookmarks page</div>
+      <Box h={'150vh'}>ini box</Box>
+    </>
+  );
+}
