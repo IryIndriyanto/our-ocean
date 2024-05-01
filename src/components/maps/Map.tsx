dfe807e225b7d12bb8aa3d1f40c24352f1afdeda
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-05-01 05:14:11 -0700

    fix client exception error

--- src/components/maps/Map.tsx
+++ src/components/maps/Map.tsx
@@ -36,9 +36,7 @@ export default function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {isLoading
-          ? null
-          : locations.map((location: ILocation) => (
+        {locations?.map((location: ILocation) => (
               <Marker
                 key={location.id}
                 position={[location.latitude, location.longitude]}
