12aee11d623888f7e7c7c6639d3b5c62ef044652
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-05-01 04:26:05 -0700

    minor change

--- src/app/main/bookmarks/page.tsx
+++ src/app/main/bookmarks/page.tsx
@@ -16,8 +16,7 @@ export default function Default() {
 
   return (
     <>
-      <div>this is bookmarks page</div>
-      <Box h={'150vh'}>ini box</Box>
+      <Box h={"150vh"}>ini box</Box>
     </>
   );
 }
