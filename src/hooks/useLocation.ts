c4e34db55c5ea8aee60a908103fe175a14dfef45
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-05-01 04:31:20 -0700

    fix for deployment

--- src/hooks/useLocation.ts
+++ src/hooks/useLocation.ts
@@ -1,7 +1,6 @@
 import useSWR, { SWRResponse } from "swr";
 import { fetcher } from "@/utils/fetcher";
-import { ILocation } from "@/types/location";
-import { locations } from '../utils/dummy.data';
+
 
 export default function useLocation() {
   const { data, error, isLoading } = useSWR(
