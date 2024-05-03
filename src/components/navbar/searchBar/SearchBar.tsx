c0e952b981ba320f65393cb2b9eecb740e4a63b7
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-05-03 06:13:55 -0700

    create responsive popover for searchbar

--- src/components/navbar/searchBar/SearchBar.tsx
+++ src/components/navbar/searchBar/SearchBar.tsx
@@ -3,12 +3,17 @@ import {
   Input,
   InputGroup,
   InputLeftElement,
+  Popover,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverTrigger,
   useColorModeValue,
 } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 import { ILocation } from "@/types/location";
-import { useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 import useLocation from "@/hooks/useLocation";
+import { InputRightElement } from "@chakra-ui/react";
 
 export function SearchBar(props: {
   variant?: string;
@@ -24,9 +29,20 @@ export function SearchBar(props: {
   const searchIconColor = useColorModeValue("gray.700", "white");
   const inputBg = useColorModeValue("secondaryGray.300", "navy.900");
   const inputText = useColorModeValue("gray.700", "gray.100");
+  const [searchTerm, setSearchTerm] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
+  const popoverRef = useRef<HTMLButtonElement>(null);
+
+  const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+  const handleInputBlur = () => {
+    popoverRef.current?.click();
+  };
+
   return (
-    <InputGroup w={{ base: "100%", md: "200px" }} {...rest}>
-      <InputLeftElement>
+    <InputGroup w={{ base: "100%", md: "300px" }} {...rest}>
+      <InputRightElement>
         <IconButton
           aria-label="search"
           bg="inherit"
@@ -41,18 +57,43 @@ export function SearchBar(props: {
           }}
           icon={<SearchIcon color={searchIconColor} w="15px" h="15px" />}
         />
-      </InputLeftElement>
-      <Input
-      id='search'
-      variant='search'
-      fontSize='sm'
-      bg={background ? background : inputBg}
-      color={inputText}
-      fontWeight='500'
-      _placeholder={{ color: 'gray.400', fontSize: '14px' }}
-      borderRadius={borderRadius ? borderRadius : '30px'}
-      placeholder={placeholder ? placeholder : 'Search...'}
-      />
+      </InputRightElement>
+      <Popover initialFocusRef={inputRef}>
+        <PopoverTrigger>
+          <Input
+            id="search"
+            variant="search"
+            fontSize="sm"
+            bg={background ? background : inputBg}
+            color={inputText}
+            fontWeight="500"
+            _placeholder={{ color: "gray.400", fontSize: "14px" }}
+            borderRadius={borderRadius ? borderRadius : "30px"}
+            placeholder={placeholder ? placeholder : "Search..."}
+            ref={inputRef}
+            value={searchTerm}
+            onChange={handleSearchChange}
+            onBlur={handleInputBlur}
+            autoComplete="off"
+          />
+        </PopoverTrigger>
+        <PopoverContent
+          borderRadius={30}
+          p={5}
+          top={3}
+          left={{ base: "10vw", md: "68px", xl: "48px" }}
+          zIndex={2}
+          h={200}
+          w={{
+            base: "80vw",
+            md: "450px",
+            xl: "430px",
+          }}
+        >
+          <PopoverCloseButton ref={popoverRef} display={"none"} />
+          {searchTerm || "No search entered"}
+        </PopoverContent>
+      </Popover>
     </InputGroup>
   );
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,51 +1,38 @@
-import { Icon } from '@chakra-ui/react';
+import { Icon } from "@chakra-ui/react";
 import {
   MdHistory,
   MdReport,
   MdHome,
   MdLock,
   MdBookmark,
-} from 'react-icons/md';
+} from "react-icons/md";
 
-import { IRoute } from '@/types/navigation';
+import { IRoute } from "@/types/navigation";
 
 const routes: IRoute[] = [
   {
-    name: 'Map',
-    layout: '/main',
-    path: '/map',
+    name: "Map",
+    layout: "/main",
+    path: "/map",
     icon: <Icon as={MdHome} width="20px" height="20px" color="inherit" />,
   },
   {
-    name: 'Bookmarks',
-    layout: '/main',
-    path: '/bookmarks',
-    icon: (
-      <Icon
-        as={MdBookmark}
-        width="20px"
-        height="20px"
-        color="inherit"
-      />
-    ),
+    name: "Bookmarks",
+    layout: "/main",
+    path: "/bookmarks",
+    icon: <Icon as={MdBookmark} width="20px" height="20px" color="inherit" />,
     secondary: true,
   },
   {
-    name: 'Recents',
-    layout: '/main',
+    name: "Recents",
+    layout: "/main",
     icon: <Icon as={MdHistory} width="20px" height="20px" color="inherit" />,
-    path: '/recents',
+    path: "/recents",
   },
   {
-    name: 'Report',
-    layout: '/main',
-    path: '/report',
-    icon: <Icon as={MdReport} width="20px" height="20px" color="inherit" />,
-  },
-  {
-    name: 'Sign In',
-    layout: '/auth',
-    path: '/sign-in',
+    name: "Sign In",
+    layout: "/auth",
+    path: "/sign-in",
     icon: <Icon as={MdLock} width="20px" height="20px" color="inherit" />,
   },
 ];
