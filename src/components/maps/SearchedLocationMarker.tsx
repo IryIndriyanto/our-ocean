94b59c711ce4389bcbdd69de62000ba578728b49
IryIndriyanto <iry.indriyanto20@gmail.com>
2024-05-08 09:31:39 -0700

    fix clint error

--- src/components/maps/SearchedLocationMarker.tsx
+++ src/components/maps/SearchedLocationMarker.tsx
@@ -1,14 +1,11 @@
+'use client'
+
 import { useSearchedLocationStore } from '@/hooks/useSearchedLocationStore'
-import { ILocation } from '@/types/location'
-import { icon, LatLngExpression } from 'leaflet'
+import { LatLngExpression } from 'leaflet'
 import { useEffect, useState } from 'react'
-import { Marker, Popup, useMapEvents } from 'react-leaflet'
+import { useMapEvents } from 'react-leaflet'
 
 export default function SearchedLocationMarker({ handleMarkerClick }: any) {
-  const ICON_CLICKED = icon({
-    iconUrl: '/assets/marker.png',
-    iconSize: [35, 35],
-  })
   const [position, setPosition] = useState<LatLngExpression | null>(null)
   const { SearchedLocation } = useSearchedLocationStore()
   const map = useMapEvents({})
